cc258c6cf03b95c603ee08fc146e382c03b16dc9
Brian M. Jemilo II <jemiloii@jemiloii.com>
2014-06-23 07:19:48 -0500

    Init Commit

---
+++ api/controllers/TradeHistoryController.js
@@ -0,0 +1,11 @@
+/**
+ * TradeHistoryController
+ *
+ * @description :: Server-side logic for managing tradehistories
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
